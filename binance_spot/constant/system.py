a46dfc87a3ee75f88da4d21803efa2fbb47b63eb
Gabor Marton <gabor.marton@ericsson.com>
2021-12-18 14:56:13 +0100

    Add binance_spot

---
+++ binance_spot/constant/system.py
@@ -0,0 +1,13 @@
+
+class WebSocketDefine:
+#    Uri = "wss://stream.binancefuture.com/ws"
+    Uri = "wss://stream.binance.com:9443/ws"
+
+class RestApiDefine:
+#     Url = "https://testnet.binancefuture.com"
+    Url = "https://api.binance.com"
+
+
+
+
+
